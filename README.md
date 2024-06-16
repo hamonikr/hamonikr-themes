ec92ccfd1a8a41092f8f05d0cbf380bf02b4541c
Kevin Kim <hckim@invesume.com>
2024-06-16 23:11:03 +0900

    Add ci action

--- README.md
+++ README.md
@@ -1,7 +1,9 @@
-## hamonikr-themes
+## HamoniKR OS Theme
 
- * 하모니카 OS 테마
- * plank theme, McMojave gtk3(https://www.gnome-look.org/p/1275087/) 등을 적용한 테마
+ * Support gtk4, gtk3, cinnamon, mate, gnome
+ * Include cursor, plank
+ * Light and Dark
+ * Include HamoniKR icon
 
 ## License
 
@@ -21,6 +23,6 @@ wget -qO- https://update.hamonikr.org/add-update-repo.apt | sudo -E bash -
 sudo apt install hamonikr-themes
 ```
 
-# 이슈 또는 버그
+# Issues or bugs
 
- 사용 중 문제를 발견하시면 root@hamonikr.org 또는 https://hamonikr.org 에서 알려주세요.
+ If you find a problem during use, please let me know in chaeya@gmail.com or https://hamonikr.org.
--- theme-test.sh
+++ theme-test.sh
@@ -1,23 +1,21 @@
 #!/bin/bash
 
-sudo cp -af usr/share/themes/HamoniKR-* /usr/share/themes/
-
-RUID=$(who | awk 'FNR == 1 {print $1}')
-RUSER_UID=$(id -u ${RUID})
+cp -af usr/share/themes/* ~/.themes/
+cp -af usr/share/icons/* ~/.icons/
 
 if [ X$1 != "Xdark" ] ; then
-    # Test HamoniKR-light theme
-    sudo -u ${RUID} DBUS_SESSION_BUS_ADDRESS="unix:path=/run/user/${RUSER_UID}/bus" gsettings set org.cinnamon.desktop.interface icon-theme 'HamoniKR'
-    sudo -u ${RUID} DBUS_SESSION_BUS_ADDRESS="unix:path=/run/user/${RUSER_UID}/bus" gsettings set org.cinnamon.desktop.interface gtk-theme 'HamoniKR'
-    sudo -u ${RUID} DBUS_SESSION_BUS_ADDRESS="unix:path=/run/user/${RUSER_UID}/bus" gsettings set org.cinnamon.desktop.wm.preferences theme 'HamoniKR'
-    sudo -u ${RUID} DBUS_SESSION_BUS_ADDRESS="unix:path=/run/user/${RUSER_UID}/bus" gsettings set org.cinnamon.theme name 'HamoniKR'
-    sudo -u ${RUID} DBUS_SESSION_BUS_ADDRESS="unix:path=/run/user/${RUSER_UID}/bus" gsettings set org.cinnamon.desktop.interface cursor-theme 'HamoniKR-Cursor'
+    # Test HamoniKR-Light theme
+    gsettings set org.cinnamon.desktop.interface icon-theme 'HamoniKR'
+    gsettings set org.cinnamon.desktop.interface gtk-theme 'HamoniKR-Light'
+    gsettings set org.cinnamon.desktop.wm.preferences theme 'HamoniKR'
+    gsettings set org.cinnamon.theme name 'HamoniKR-Light'
+    gsettings set org.cinnamon.desktop.interface cursor-theme 'HamoniKR'
 
 else
-    # Test HamoniKR-dark theme
-    sudo -u ${RUID} DBUS_SESSION_BUS_ADDRESS="unix:path=/run/user/${RUSER_UID}/bus" gsettings set org.cinnamon.desktop.interface icon-theme 'HamoniKR'
-    sudo -u ${RUID} DBUS_SESSION_BUS_ADDRESS="unix:path=/run/user/${RUSER_UID}/bus" gsettings set org.cinnamon.desktop.interface gtk-theme 'HamoniKR-dark'
-    sudo -u ${RUID} DBUS_SESSION_BUS_ADDRESS="unix:path=/run/user/${RUSER_UID}/bus" gsettings set org.cinnamon.desktop.wm.preferences theme 'HamoniKR-dark'
-    sudo -u ${RUID} DBUS_SESSION_BUS_ADDRESS="unix:path=/run/user/${RUSER_UID}/bus" gsettings set org.cinnamon.theme name 'HamoniKR-dark'    
-    sudo -u ${RUID} DBUS_SESSION_BUS_ADDRESS="unix:path=/run/user/${RUSER_UID}/bus" gsettings set org.cinnamon.desktop.interface cursor-theme 'HamoniKR-Cursor' 
+    # Test HamoniKR-Dark theme
+    gsettings set org.cinnamon.desktop.interface icon-theme 'HamoniKR'
+    gsettings set org.cinnamon.desktop.interface gtk-theme 'HamoniKR-Dark'
+    gsettings set org.cinnamon.desktop.wm.preferences theme 'HamoniKR'
+    gsettings set org.cinnamon.theme name 'HamoniKR-Dark'    
+    gsettings set org.cinnamon.desktop.interface cursor-theme 'HamoniKR' 
 fi 
